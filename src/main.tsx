84acffc68cedcc68f1d354e4da3849202b82dca5
Adesegha Cyril <39329297+kingbasco@users.noreply.github.com>
2026-09-22 00:46:22 +0100

    Fix screening stylesheet import

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import './styles.css'\nimport './screening-review.css'
+import './styles.css'
+import './screening-review.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
