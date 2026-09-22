96bea298a5e3c7afa3400433f1b7b4a8779575bd
Adesegha Cyril <39329297+kingbasco@users.noreply.github.com>
2026-09-22 02:35:14 +0100

    Polish form settings UI

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './screening-review.css'
+import './form-settings-polish.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
